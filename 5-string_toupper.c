11274b6d99e7712fa811ec970a0fcea76fc2450f
PrincessGloria <110991033+PrincessGloria@users.noreply.github.com>
2022-09-21 13:47:05 +0100

    Create 5-string_toupper.c

---
+++ 5-string_toupper.c
@@ -0,0 +1,24 @@
+julien@ubuntu:~/0x06$ cat 5-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char str[] = "Look up!\n";
+    char *ptr;
+
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
+}
+julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-string_toupper.c -o 5-string_toupper
+julien@ubuntu:~/0x06$ ./5-string_toupper 
+LOOK UP!
+LOOK UP!
+julien@ubuntu:~/0x06$
